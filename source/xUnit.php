b13b0611a6e12433a432fd09bcc4ee656fd2eca0
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-04-19 10:15:58 +0200

    Chapter 23: Counting

--- source/xUnit.php
+++ source/xUnit.php
@@ -15,15 +15,7 @@ class TestCaseTest
         assert('1 run, 0 failed' == $result->summary(), 'Expected "1 run, 0 failed" actual: ' . $result->summary());
     }
 
-    public function testFailedResult()
-    {
-        $test = new WasRun('testBrokenMethod');
-        $result = $test->run();
-        assert('1 run, 1 failed', $result->summary());
-    }
-
 }
 
 $test = new TestCaseTest();
 $test->testTemplateMethod();
-$test->testFailedResult();
