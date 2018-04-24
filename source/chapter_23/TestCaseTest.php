7249e5ad1bb0ff595556b0c833ecab21eaf3a3ac
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-04-24 18:48:59 +0200

    Chapter 23: Counting

--- source/chapter_23/TestCaseTest.php
+++ source/chapter_23/TestCaseTest.php
@@ -11,4 +11,18 @@ class TestCaseTest extends TestCase
         assert('1 run, 0 failed' == $result->summary(), 'Expected "1 run, 0 failed". Actual: ' . $result->summary());
     }
 
+    public function testResult()
+    {
+        $test = new WasRun('testMethod');
+        $result = $test->run();
+        assert('1 run, 0 failed' == $result->summary(), 'Expected "1 run, 0 failed". Actual: ' . $result->summary());
+    }
+
+    public function testFailedResult()
+    {
+        $test = new WasRun('testBrokenMethod');
+        $result = $test->run();
+        assert('1run, 1 failed' == $result->summary(), 'Expected "1 run, 0 failed". Actual: ' . $result->summary());
+    }
+
 }
--- source/chapter_23/main.php
+++ source/chapter_23/main.php
@@ -6,3 +6,9 @@ use chapter_23\TestCaseTest;
 
 $test = new TestCaseTest('testTemplateMethod');
 $test->run();
+
+$test = new TestCaseTest('testResult');
+$test->run();
+
+$test = new TestCaseTest('testFailedResult');
+$test->run();
